8d377e320a3a36011b1eaf17ed755e7a12063a1c
Matt <mattdahepic@gmail.com>
2015-02-04 09:47:08 -0700

    ready for distribution?

--- src/main/java/com/mattdahepic/oredictconv/OreDictConv.java
+++ src/main/java/com/mattdahepic/oredictconv/OreDictConv.java
@@ -19,7 +19,7 @@ public class OreDictConv {
 
     public static final String MODID = "oredictconv";
     public static final String NAME = "Ore Dictionary Converter";
-    public static final String VERSION = "v0.5";
+    public static final String VERSION = "1.0";
 
     @SidedProxy(clientSide = "com.mattdahepic.oredictconv.client.ClientProxy",serverSide = "com.mattdahepic.oredictconv.CommonProxy")
     public static CommonProxy proxy;
